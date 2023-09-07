15b6e25fab8881809efb1f09dca9c61c1bfaa499
Pro8 <111078922+Pro8-coder@users.noreply.github.com>
2023-09-07 20:07:22 +0300

    Initial commit

---
+++ home_work_01/index.js
@@ -0,0 +1,9 @@
+let temperatureCelsius = 23;
+const temperatureFahrenheit = (9 / 5) * temperatureCelsius + 32;
+
+console.log(`Температура ${temperatureCelsius} по Цельсию или ${temperatureFahrenheit} по Фаренгейту.`);
+
+let name = "Александр";
+let admin = name;
+
+console.log(admin);
